0e1a5588ca6a9fe71406df0219a08e4603bb9f7f
Maarcoos0 <164019718+Maarcoos0@users.noreply.github.com>
2024-06-18 09:58:56 -0300

    Add files via upload

--- converções.js
+++ converções.js
@@ -1,8 +1,15 @@
 // Tipo  de dade
 // Booleanos
 
-//conversão implícita
+//conversao Implicita
 const numero = 456;
 const numeroString = Number("456a");
 
 console.log(numero + numeroString)
+
+
+//Conversao implicita
+//Acontecer quando o javascript converte o numero em string e depois
+//as 2 strings
+console.log(numero == numeroString);
+console.log(numero + numeroString);
